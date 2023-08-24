44fd6f94faf3888b3b4119fe63850a4d244aaca6
Dennie <oculareo@gmail.com>
2023-08-24 19:58:23 -0400

    Added recipe_list view, url and html page

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import show_recipe
+from recipes.views import show_recipe, recipe_list
 
 urlpatterns = [
+    path("recipes/", recipe_list),
     path("recipes/<int:id>/", show_recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,13 @@ from recipes.models import Recipe
 def show_recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id)
     context = {
-        "recipe_object": recipe
+        "recipe_object": recipe,
     }
     return render(request, "recipes/detail.html", context)
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    context = {
+        "recipe_list": recipes,
+    }
+    return render(request, "recipes/list.html", context)
