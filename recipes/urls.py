c5056ed4f787c3580a9256967b2bbf6df41b2899
Dennie <oculareo@gmail.com>
2023-08-28 21:52:03 -0400

    Revised project and app url names

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from recipes.views import show_recipe, recipe_list, create_recipe, edit_recipe
 
 urlpatterns = [
-    path("recipes/", recipe_list, name="recipe_list"),
-    path("recipes/<int:id>/", show_recipe, name="show_recipe"),
-    path("recipes/<int:id>/edit", edit_recipe, name="edit_recipe"),
-    path("recipes/create/", create_recipe, name="create_recipe")
+    path("", recipe_list, name="recipe_list"),
+    path("<int:id>/", show_recipe, name="show_recipe"),
+    path("<int:id>/edit", edit_recipe, name="edit_recipe"),
+    path("create/", create_recipe, name="create_recipe")
 ]
