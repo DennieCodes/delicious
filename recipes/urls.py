e76ee87a903edb75f0d343a45b1f89b8bef0b186
Dennie <oculareo@gmail.com>
2023-08-28 21:46:30 -0400

    Added edit recipe functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import show_recipe, recipe_list, create_recipe
+from recipes.views import show_recipe, recipe_list, create_recipe, edit_recipe
 
 urlpatterns = [
     path("recipes/", recipe_list, name="recipe_list"),
     path("recipes/<int:id>/", show_recipe, name="show_recipe"),
+    path("recipes/<int:id>/edit", edit_recipe, name="edit_recipe"),
     path("recipes/create/", create_recipe, name="create_recipe")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -34,3 +34,23 @@ def create_recipe(request):
     }
 
     return render(request, "recipes/create.html", context)
+
+# EDIT_RECIPE
+def edit_recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+
+            return redirect("show_recipe", id=id)
+
+    else:
+        form = RecipeForm(instance=recipe)
+
+    context = {
+        "recipe_object": recipe,
+        "form": form,
+    }
+    return render(request, "recipes/edit.html", context)
